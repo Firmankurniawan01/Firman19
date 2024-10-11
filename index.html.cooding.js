9c2d8a8941b4469929b56401b6180337e49e0577
Firmankurniawan01 <zawusupu169@gakew.com>
2024-10-11 11:53:13 +0700

    Add files via upload

---
+++ index.html.cooding.js
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+   <!-- PROMOTION COMPONENT -->
+   <div class="bg-indigo-600 px-4 py-3 text-white">
+    <p class="text-center text-sm font-medium">
+      PROMO HINGGA 60%
+      <a href="#" class="inline-block underline">BERLAKU UNTUK HARI INI SAJA!</a>
+    </p>
+  </div>
+   <!-- END PROMOTION COMPONENT -->
+
+   <!-- NAVBAR COMPONENT -->
+   <header>
+    <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <div class="flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
+        <div>
+          <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">FIRMAN STORE</h1>
+  
+          <p class="mt-1.5 text-sm text-gray-500">
+            Belanja koleksi langka % Murah.
+          </p>
+        </div>
+  
+        <div class="flex items-center gap-4">
+          <button
+            class="inline-flex items-center justify-center gap-1.5 rounded border border-gray-200 bg-white px-5 py-3 text-gray-900 transition hover:text-gray-700 focus:outline-none focus:ring"
+            type="button"
+            <span class="text-sm font-medium"> Cara Belanja </span>
+          </button>
+  
+          <button
+            class="inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
+            type="button"
+          >
+            Chat admin
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+    <!-- END NAVBAR COMPONENT -->
+
+   <!-- CALL TO ACTION COMPONENT --> 
+<section class="bg-gray-50">
+    <div class="p-8 md:p-12 lg:px-16 lg:py-24">
+      <div class="mx-auto max-w-lg text-center">
+        <h2 class="text-2xl font-bold text-gray-900 md:text-3xl">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit
+        </h2>
+  
+        <p class="hidden text-gray-500 sm:mt-4 sm:block">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae dolor officia blanditiis
+          repellat in, vero, aperiam porro ipsum laboriosam consequuntur exercitationem incidunt
+          tempora nisi?
+        </p>
+      </div>
+  
+      <div class="mx-auto mt-8 max-w-xl">
+        <form action="#" class="sm:flex sm:gap-4">
+          <div class="sm:flex-1">
+            <input
+              type="TEXT"
+              placeholder="Lemari jam dinding langka..."
+              class="w-full rounded-md border-gray-200 bg-white p-3 text-gray-700 shadow-sm transition focus:border-white focus:outline-none focus:ring focus:ring-indigo-400"
+            />
+          </div>
+  
+          <button
+            type="submit"
+            class="group mt-4 flex w-full items-center justify-center gap-2 rounded-md bg-indigo-600 px-5 py-3 text-white transition focus:outline-none focus:ring focus:ring-indigo-400 sm:mt-0 sm:w-auto"
+          >
+            <span class="text-sm font-medium"> CARI BARANG </span>
+  
+            <svg
+              class="size-5 rtl:rotate-180"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </button>
+        </form>
+      </div>
+    </div>
+  </section>
+s  
+    <!-- END CALL TO ACTION COMPONENT -->
+
+    <!-- PRODUCT LIST COMPONENT -->
+       <div class="grid grid-cols-3">
+       <a href="htpps://wa.me/6281387709084?text=Firman store saya mau beli produk vintage clock" class="group block">
+            <img
+                      src="https://media.istockphoto.com/id/145926252/id/foto/jam-saku-vintage-di-latar-belakang-putih.webp?s=1024x1024&w=is&k=20&c=NdhG34W5XIvVKVZPoXEVDKTvJzrGfv9d9otVMznIXGw="
+              alt=""
+              class="aspect-square w-full rounded object-cover"
+            />
+          
+            <div class="mt-3">
+              <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                Vintage clock
+              </h3>
+          
+              <p class="mt-1 text-sm text-gray-700">$Rp 1.500.000</p>
+            </div>
+
+          </a>       <div class="grid grid-cols-3">
+       <a href="htpps://wa.me/6281387709084?text=Firman store saya mau beli produk vintage clock" class="group block">
+            <img
+                      src="https://media.istockphoto.com/id/145926252/id/foto/jam-saku-vintage-di-latar-belakang-putih.webp?s=1024x1024&w=is&k=20&c=NdhG34W5XIvVKVZPoXEVDKTvJzrGfv9d9otVMznIXGw="
+              alt=""
+              class="aspect-square w-full rounded object-cover"
+            />
+          
+            <div class="mt-3">
+              <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+                Vintage clock
+              </h3>
+          
+              <p class="mt-1 text-sm text-gray-700">$Rp 1.500.000</p>
+          
+       </div>a href="htpps://wa.me/6281387709084?text=Firman store saya mau beli produk mesin ketik jadul 1911" class="group block">
+       <img
+                 src="https://images.unsplash.com/photo-1695747003514-83297acc8de3?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+         alt=""
+         class="aspect-square w-full rounded object-cover"
+       />
+     
+       <div class="mt-3">
+         <h3 class="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+           Mesin ketik jadul 1911
+         </h3>
+     
+         <p class="mt-1 text-sm text-gray-700">$Rp 10.500.000</p>
+       </div>
+     </a>
+
+    <!-- END PRODUCT LIST COMPONENT -->
+</body>
+</html>
